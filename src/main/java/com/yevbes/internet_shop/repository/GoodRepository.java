6048eda5ac129710f3abb10e6204335699e7125b
Aleslong <yevbes@gmail.com>
2024-12-12 17:45:42 +0100

    Add repositories

--- src/main/java/com/yevbes/internet_shop/repository/GoodRepository.java
+++ src/main/java/com/yevbes/internet_shop/repository/GoodRepository.java
@@ -4,8 +4,6 @@ import com.yevbes.internet_shop.model.Good;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface GoodRepository extends JpaRepository<Good, Long> {
 
